f5f1b28bea290e129a74b125d65627a248cd3550
harryscully <harry.scully@ugogroup.co.uk>
2026-03-10 15:51:07 +0000

    Style active link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,28 @@
 "use client"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
 import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
 
+const navLinks = [
+    { displayName: "Dashboard", href: "/" },
+    { displayName: "Tasks", href: "/tasks" }
+]
+
 export default function Navbar() {
+    const pathname = usePathname()
     return (
-       <NavigationMenu className="w-fit">
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link href="/">Dashboard</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                        <Link href="/tasks">Tasks</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
+        <NavigationMenu className="w-fit">
+            <NavigationMenuList className="flex gap-2">
+                {navLinks.map((link) => (
+                    <NavigationMenuItem key={link.displayName}>
+                        <NavigationMenuLink asChild className={cn(navigationMenuTriggerStyle(), pathname === link.href && "font-extrabold")}>
+                            <Link href={link.href}>
+                                {link.displayName}
+                            </Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                ))}
             </NavigationMenuList>
         </NavigationMenu>
     )
