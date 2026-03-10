c72d3a5caa87f5103104981377b5f2127a381a3c
harryscully <harry.scully@ugogroup.co.uk>
2026-03-10 10:01:33 +0000

    Add KanbanCard component

---
+++ src/components/KanbanCard.tsx
@@ -0,0 +1,24 @@
+import { Badge } from "./ui/badge";
+import { Card, CardHeader, CardContent, CardTitle } from "./ui/card";
+import type { Task } from "@/data/tasks";
+
+const priorityVariant: Record<Task["priority"], string> = {
+    "high": "bg-red-950 text-red-300",
+    "medium": "bg-blue-950 text-blue-300",
+    "low": "bg-green-950 text-green-300"
+}
+
+export default function KanbanCard({ task }: { task: Task }) {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    {task.title}
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <Badge className={`uppercase ${priorityVariant[task.priority]}`}>{task.priority}</Badge>
+            </CardContent>
+        </Card>
+    )
+}
