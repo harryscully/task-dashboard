63889654d6a4dbcc9004422794bd4f5a40cee961
harryscully <harry.scully@ugogroup.co.uk>
2026-03-13 12:43:06 +0000

    Add Zod form validation schema for new task

---
+++ src/lib/schemas.ts
@@ -0,0 +1,11 @@
+import * as z from "zod"
+
+export const taskSchema = z.object({
+    title: z.string().min(1,"Title is required"),
+    description: z.optional(z.string()),
+    priority: z.enum(["HIGH","MEDIUM","LOW"]),
+    dueDate: z.optional(z.date()),
+    columnId: z.string()
+})
+
+export type TaskSchema = z.infer<typeof taskSchema>
